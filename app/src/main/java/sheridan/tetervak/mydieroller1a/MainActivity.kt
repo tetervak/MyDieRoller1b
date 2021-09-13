d515d52221078655176c179f55394ed2e95d8cd1
Alexander Tetervak <tetervak@sheridancollege.ca>
2021-09-13 08:47:24 -0400

    initial commit

---
+++ app/src/main/java/sheridan/tetervak/mydieroller1a/MainActivity.kt
@@ -0,0 +1,11 @@
+package sheridan.tetervak.mydieroller1a
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
