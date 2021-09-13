e7c00b858274e57aabfe850a0155d0e4f1cc4ad7
Alexander Tetervak <tetervak@sheridancollege.ca>
2021-09-13 10:33:04 -0400

    used view binding

--- app/src/main/java/sheridan/tetervak/mydieroller1a/MainActivity.kt
+++ app/src/main/java/sheridan/tetervak/mydieroller1a/MainActivity.kt
@@ -3,9 +3,8 @@ package sheridan.tetervak.mydieroller1a
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
+import sheridan.tetervak.mydieroller1a.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,39 +13,39 @@ class MainActivity : AppCompatActivity() {
         const val RESULT = "Result"
     }
 
-    var dieValue : Int = 0
+    private var dieValue : Int = 0
+
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         if(savedInstanceState is Bundle){
             val savedResult : Int = savedInstanceState.getInt(RESULT)
             if(savedResult > 0){
-                val result: TextView = findViewById(R.id.result)
-                result.text = savedResult.toString()
+                binding.result.text = savedResult.toString()
                 dieValue = savedResult
             }
         }
 
-        val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.setOnClickListener {
+        binding.rollButton.setOnClickListener {
             Log.d(TAG, "The button is clicked")
             onRoll()
         }
     }
 
     private fun onRoll() {
-        val result: TextView = findViewById(R.id.result)
         dieValue = (1..6).random()
-        result.text = dieValue.toString()
+        binding.result.text = dieValue.toString()
         Toast.makeText(this, getString(R.string.rolled), Toast.LENGTH_LONG).show()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         if(dieValue > 0){
-            outState.putInt(RESULT, dieValue);
+            outState.putInt(RESULT, dieValue)
         }
     }
 }
