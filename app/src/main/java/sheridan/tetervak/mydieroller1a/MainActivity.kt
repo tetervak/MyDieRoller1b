ef9d6535a548bdbd597f524999a026770e92b926
Alexander Tetervak <tetervak@sheridancollege.ca>
2021-09-27 06:33:42 -0400

    onClick in the databinding

--- app/src/main/java/sheridan/tetervak/mydieroller1a/MainActivity.kt
+++ app/src/main/java/sheridan/tetervak/mydieroller1a/MainActivity.kt
@@ -2,17 +2,10 @@ package sheridan.tetervak.mydieroller1a
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.viewModels
 import sheridan.tetervak.mydieroller1a.databinding.ActivityMainBinding
-import sheridan.tetervak.mydieroller1a.model.Die
 
 class MainActivity : AppCompatActivity() {
-
-    companion object{
-        const val TAG = "MainActivity"
-    }
 
     private val viewModel: MainViewModel by viewModels()
 
@@ -23,21 +16,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.viewModel = viewModel
         viewModel.dieValue.observe(this){
             if(it > 0){
                 binding.result.text = it.toString()
             }
         }
-
-        binding.rollButton.setOnClickListener {
-            Log.d(TAG, "The button is clicked")
-            onRoll()
-        }
-    }
-
-    private fun onRoll() {
-        viewModel.roll()
-        Toast.makeText(this, getString(R.string.rolled), Toast.LENGTH_LONG).show()
     }
 
 }
